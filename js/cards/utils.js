70c0b85d8095bdced2029b4d69b7a6e7d6c92999
Monclus Maria Elisa <monclus@gmail.com>
2023-06-09 13:42:45 -0300

    Add files via upload

---
+++ js/cards/utils.js
@@ -0,0 +1,33 @@
+// UTILS
+
+
+const colors = ['bg-danger','bg-info',  'bg-success']
+
+const transparent = 'bg-transparent'
+
+const changeColor = (container, index, revert = false) => {
+
+    const i = Number(index)
+
+    revert
+        ? container.classList.replace(colors[i], transparent)
+        : container.classList.replace(transparent, colors[i])
+}
+
+
+const matchCategory = (selection) => {
+
+    switch (selection) {
+        case "0":
+            form.category.value = 'a'
+            break
+        case "1":
+            form.category.value = 'b'
+            break
+        case "2":
+            form.category.value = 'c'
+            break
+        default:
+            throw new Error('Algo pasó en Match Category')
+    }
+}
